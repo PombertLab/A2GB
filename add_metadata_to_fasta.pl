eab4f1968c18c0d27e36b14082af0bee5032acde
Jean-Francois Pombert <jpombert@iit.edu>
2020-10-21 13:37:26 -0500

    added padding zeroes

--- add_metadata_to_fasta.pl
+++ add_metadata_to_fasta.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 ## Pombert Lab, IIT, 2020
 my $name = 'add_metadata_to_fasta.pl';
-my $version = '0.1';
+my $version = '0.2';
 
 use strict; use warnings; use Getopt::Long qw(GetOptions);
 
@@ -20,6 +20,7 @@ OPTIONS:
 -g (--gcode)		NCBI genetic code [Default: 1]
 -m (--moltype)		NCBI moltype descriptor [Default: genomic]
 -c (--chromosome)	Annotate contigs as chromosomes
+-w (--width)		Character width for chromosome numbers [Default: 2] ## Adds padding zeroes if below threshold
 
 OPTIONS
 die "$usage\n" unless @ARGV;
@@ -31,7 +32,8 @@ my $strain;
 my $lineage;
 my $gcode = 1;
 my $moltype = 'genomic';
-my $chromosome = 0;
+my $chromosome;
+my $cw = 2;
 
 GetOptions(
 	'f|fasta=s@{1,}' => \@fasta,
@@ -43,14 +45,17 @@ GetOptions(
 	'c|chromosome' => \$chromosome
 );
 
+my $chr; my $chrname = '';
 while (my $file = shift@fasta){
 	open IN, "<$file";
 	open OUT, ">$file.headers";
-	$chromosome++;
+	$chr++;
+	$chr = sprintf("%0${cw}d", $chr);
+	if ($chromosome){$chrname = '[chromosome=chromosome_'."$chr".']';}
 	while (my $line = <IN>){
 		chomp $line;
 		if ($line =~ /^>(\S+)/){
-			print OUT ">$1 [organism=$organism][strain=$strain][lineage=$lineage][gcode=$gcode][moltype=$moltype][chromosome=$chromosome]\n";
+			print OUT ">$1 [organism=$organism][strain=$strain][lineage=$lineage][gcode=$gcode][moltype=$moltype]"."$chrname"."\n";
 		}
 		else {print OUT "$line\n";}
 	}
