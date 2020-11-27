a852128f6a3e1a3185998bbe0ffd4212c079d95f
Jean-Francois Pombert <jpombert@iit.edu>
2020-11-27 09:49:27 -0600

    Update splitMakerGFF.pl

--- Apollo_tools/splitMakerGFF.pl
+++ Apollo_tools/splitMakerGFF.pl
@@ -1,23 +1,34 @@
 #!/usr/bin/perl
+## Pombert Lab, IIT, 2017
+my $name = 'splitMakerGFF.pl';
+my $version = '0.1'; 
 
-use strict;
-use warnings;
+use strict; use warnings;
 
-my $usage = 'perl splitMakerGFF.pl *.gff';
+my $usage = <<"OPTIONS";
+NAME		$name
+VERSION		$version
+SYNOPSIS	Splits the augustus, genemark and repeatmasker entries into ones; useful for loading them as separate tracks in WebApollo.
+USAGE		splitMakerGFF.pl *.gff
+OPTIONS
 die "$usage\n" unless @ARGV;
 
 while(my $file = shift@ARGV){
 	open IN, "<$file";
 	$file =~ s/.gff$//;
-	open OUT1, ">$file.genes.gff";
-	open OUT2, ">$file.repeats.gff";
+	open OUT1, ">$file.augustus.gff";
+	open OUT2, ">$file.genemark.gff";
+	open OUT3, ">$file.repeats.gff";
 	while (my $line = <IN>){
 		chomp $line;
 		if ($line =~ /^\S+\taugustus_masked/){
 			print OUT1 "$line\n";
 		}
-		elsif ($line =~ /^\S+\trepeatmasker/){
+		elsif ($line =~ /^\S+\tgenemark/){
 			print OUT2 "$line\n";
+		}
+		elsif ($line =~ /^\S+\trepeatmasker/){
+			print OUT3 "$line\n";
 		}
 	}
 }
