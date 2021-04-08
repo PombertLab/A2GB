e4a86ea17563a5c7b94fd90a87746097c86bd33a
Pombert-JF <jpombert@iit.edu>
2021-04-08 12:20:06 -0500

    readability

--- Function_prediction/get_queries.pl
+++ Function_prediction/get_queries.pl
@@ -2,24 +2,31 @@
 ## Pombert Lab, IIT, 2017
 my $name = 'get_queries.pl';
 my $version = '0.1';
+my $updated = '2021-04-08';
 
 use strict; use warnings;
 
 my $usage = <<"OPTIONS";
-
-NAME		$name
-VERSION		$version
+NAME		${name}
+VERSION		${version}
+UPDATED		${updated}
 SYNOPSIS	Generates simple lists of sequences present in FASTA files
+
 USAGE		get_queries.pl *.fasta
 OPTIONS
-die "$usage\n" unless @ARGV;
+die "\n$usage\n" unless @ARGV;
 
 while (my $fasta = shift@ARGV){
-	open IN, "<$fasta";
-	$fasta =~ s/.fasta//; $fasta =~ s/.fsa//;
-	open OUT, ">$fasta.queries";
-	while (my $line = <IN>){
+
+	open FASTA, "<", "$fasta" or die "Can't open $fasta: $!\n";
+	$fasta =~ s/\.\w+$//;
+	open OUT, ">", "$fasta.queries" or die "Can't create $fasta.queries: $!\n";
+
+	while (my $line = <FASTA>){
 		chomp $line;
-		if ($line =~ /^>(\S+)/){print OUT "$1\n";}
+		if ($line =~ /^>(\S+)/){
+			my $query = $1;
+			print OUT "$query\n";
+		}
 	}
 } 
