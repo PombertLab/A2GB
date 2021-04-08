ecf44d80b2c4dab48c98f2eddf4822b8f8f8e84a
Pombert-JF <jpombert@iit.edu>
2021-04-08 12:25:09 -0500

    readability fix

--- Function_prediction/get_reference_products.pl
+++ Function_prediction/get_reference_products.pl
@@ -2,21 +2,25 @@
 ## Pombert Lab, IIT, 2020
 my $name = 'get_reference_products.pl';
 my $version = '0.1';
+my $updated = '2021-04-08';
 
 use strict; use warnings; use PerlIO::gzip; use Getopt::Long qw(GetOptions);
 
 my $usage = <<"OPTIONS";
-
-NAME		$name
-VERSION		$version
+NAME		${name}
+VERSION		${version}
+UPDATED		${updated}
 SYNOPSIS	Creates a single tab-delimited list of products NCBI protein (.faa) FASTA files
-USAGE		$name -f *.gz -l reference.list
+
+USAGE		${name} \\
+		  -f *.gz \\
+		  -l reference.list
 
 OPTIONS:
 -f (--fasta)	NCBI protein FASTA files 
 -l (--list)	Desired output list name
 OPTIONS
-die "$usage\n" unless @ARGV;
+die "\n$usage\n" unless @ARGV;
 
 my $list;
 my @fasta;
@@ -25,26 +29,37 @@ GetOptions(
 	'f|fasta=s@{1,}' => \@fasta
 );
 
-open OUT, ">", "$list"; 
+open OUT, ">", "$list" or die "Can't create $list: $!\n"; 
 while (my $file = shift@fasta){
-	my $fh; my $format;
-	my $stime  = time;
+
+	my $fh;
+	my $format;
+	my $stime = time;
+
 	if ($file =~ /.gz$/){ ## Autodecting if file is gzipped from the file extension
-		open $fh, "<:gzip", "$file" or die "Could not open $file for reading: $!\n";
+		open $fh, "<:gzip", "$file" or die "Can't open $file: $!\n";
 		$format = 'gzip';
-		$file =~ s/.faa.gz$//; $file =~ s/.fasta.gz$//; $file =~ s/.gz$//;
+		$file =~ s/.faa.gz$//;
+		$file =~ s/.fasta.gz$//;
+		$file =~ s/.gz$//;
 	}
-	else{
-		open $fh, "<$file" or die "Could not open $file for reading: $!\n"; 
+	else {
+		open $fh, "<", "$file" or die "Can't open $file: $!\n"; 
 		$format = 'fasta';
 		$file =~ s/.\w+$//;
 	}
+
 	print "Extracting information from $file. This might take a while...\n";
 	while (my $line = <$fh>){
 		chomp $line;
-		if ($line =~ /^>(\S+)\s(.*)\s\[/){print OUT "$1\t$2\n";}
+		if ($line =~ /^>(\S+)\s(.*)\s\[/){
+			my $locus = $1;
+			my $product = $2;
+			print OUT "$locus\t$product\n";
+		}
 	}
-	if ($format eq 'gzip'){binmode $fh, ":gzip(none)";}
+	
+	if ($format eq 'gzip'){ binmode $fh, ":gzip(none)"; }
 	my $runtime = time - $stime;
 	print "Time to extract products from file $file: $runtime seconds.\n";
 }
